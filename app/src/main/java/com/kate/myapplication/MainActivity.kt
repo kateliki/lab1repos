84a722003b5eff0cda57f5b0f3c8b67a64afa588
likinceva051289@gmail.com <you@example.com>
2021-11-23 19:34:39 +0400

    initial commit

---
+++ app/src/main/java/com/kate/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kate.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        hjfljhflhvjkb;kjb;
+    }
+}
